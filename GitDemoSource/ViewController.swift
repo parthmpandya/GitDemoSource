a39e40b22f94a97b07f817c571860782233cf272
Parth Pandya <Parth@jg-pc12.net4nuts.local>
2018-06-20 15:33:30 +0530

    Initial Commit

---
+++ GitDemoSource/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemoSource
+//
+//  Created by Parth Pandya on 20/06/18.
+//  Copyright © 2018 Parth Pandya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
